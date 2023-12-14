893451a9ebd9c6dbf33a1c37fa90ed9a2387d34f
YsN23 <yy2369964@gmail.com>
2023-12-14 18:53:01 +0100

    Done

---
+++ stack_nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @new_node: double pointer to the node
+ * @l_num: line number of of the opcode.
+ */
+void nop(stack_t **new_node, unsigned int l_num)
+{
+	(void)new_node;
+	(void)l_num;
+}
